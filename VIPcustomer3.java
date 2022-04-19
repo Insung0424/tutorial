1914b35f0eac8dd6ede9f81814096a8c068986b3
Insung0424 <zhrudrhks@gmail.com>
2022-04-19 17:32:15 +0900

    seventh commit

---
+++ VIPcustomer3.java
@@ -0,0 +1,36 @@
+package ch04;
+
+public class VIPcustomer extends Customer{
+		
+		String agentId;
+		double saleRatio;
+		
+//		public VIPcustomer() {
+////			super(0, "no-name");
+//			bonusRatio = 0.05;
+//			saleRatio = 0.1;
+//			customerGrade = "VIP";
+//			
+//			System.out.println("VIPcustomer() call");
+//		}
+		
+		public VIPcustomer(int customerId, String customerName) {
+			super(customerId, customerName);
+			
+			customerGrade = "VIP";
+			bonusRatio = 0.05;
+			saleRatio = 0.1;
+		}
+		
+		@Override
+		public int calcPrice(int price) {
+			bonusPoint += price * bonusRatio;
+			price -= (int)(price * saleRatio);
+			return price;
+		}
+
+		public String getAgentId() {
+			return agentId;
+		}
+		
+}
