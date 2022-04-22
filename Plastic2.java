fe516ed9aa3ef0dc82c0f885aa261e67df5cbd26
Insung0424 <zhrudrhks@gmail.com>
2022-04-22 20:48:10 +0900

    sixth commit

---
+++ Plastic2.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class Plastic extends Material{
+		public String toString() {
+			return "material is plastic";
+	}
+
+		@Override
+		public void doPrinting() {
+			// TODO Auto-generated method stub
+			
+		}
+}
