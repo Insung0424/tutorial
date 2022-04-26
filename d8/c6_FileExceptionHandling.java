347d614da6770cf1b1c58823b205498738fc9945
Insung0424 <zhrudrhks@gmail.com>
2022-04-26 17:23:27 +0900

    fourth commit

---
+++ d8/c6_FileExceptionHandling.java
@@ -0,0 +1,31 @@
+package ch08;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExceptionHandling {
+
+	public static void main(String[] args) {
+
+			FileInputStream fis = null;
+			
+			try {
+				fis = new FileInputStream("a.txt");
+				System.out.println("read");
+			} catch (FileNotFoundException e) {
+				System.out.println(e);
+			} finally {
+				if(fis != null)
+					try {
+						fis.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+				}
+				System.out.println("pass");
+			}
+			System.out.println("end");
+	}
+
+}
